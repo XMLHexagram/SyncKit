a0d89b1efdc3e57282ccd801805a7114b93826d2
XMLHexagram <52130356+XMLHexagram@users.noreply.github.com>
2022-12-18 21:43:32 +0800

    Update ServerToken.swift

--- SyncKit/Classes/RealmSwift/ServerToken.swift
+++ SyncKit/Classes/RealmSwift/ServerToken.swift
@@ -9,6 +9,10 @@ import Foundation
 import RealmSwift
 
 class ServerToken: Object {
-    
+    @objc dynamic var id = UUID().uuidString
     @objc dynamic var token: Data? = nil
+    
+    override class func primaryKey() -> String? {
+        return "id"
+    }
 }
